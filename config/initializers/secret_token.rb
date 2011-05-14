545f1d8c4cac29fec26b16bea6993d7af8dbdb40
Mark Muskardin <markmuskardin@gmail.com>
2011-05-14 12:52:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishs3::Application.config.secret_token = '55218ef662a8d14afcc39fd4343c20df3949f5febd05aa94fe705c60d6541cc318147e038c84be7445359889294f994dfde6b9407e250be0dbdb8ff476cf1f69'
